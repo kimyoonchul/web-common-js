7f694d605d02041ec332fc890f01e8a1360b5762
김윤철 김윤철 <chuli8944@gmail.com>
2018-09-11 10:04:31 +0900

    number_format is_null
    add function

--- web-common-js/WebContent/js/common.js
+++ web-common-js/WebContent/js/common.js
@@ -1,3 +1,12 @@
-function test(){
-	alert();
+// ex) 10000 -> 10,000 like php number_format
+function number_format(number){
+	return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+// null check function like php ==
+function is_null(value){
+	if( value == "" || value == null || value == undefined || ( value != null && typeof value == "object" && !Object.keys(value).length ) ){ 
+		return true 
+	}else{ 
+		return false 
+	}
 }
